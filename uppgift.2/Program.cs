10b420a234d159399163034744142e97585d69f2
Admin <Admin@TEB127001>
2023-01-18 10:41:41 +0100

    Add project files.

---
+++ uppgift.2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace uppgift2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("uppgift 2");
+        }
+    }
+}
+
+
